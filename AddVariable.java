538107ef2d3e2fba058496b6046eb975026ca095
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-11 15:01:26 -0500

    Create AddVariable.java

---
+++ AddVariable.java
@@ -0,0 +1,9 @@
+public class AddVariable {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 3;
+        int resutlt = num1 + num2;
+        System.out.println(resutlt);
+    }
+}
