8398a8206b2bff991e7dfcffeb7b9155b31dd843
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-12 21:21:37 -0500

    Create TypePromotion.java

---
+++ TypePromotion.java
@@ -0,0 +1,11 @@
+public class TypePromotion {
+    public static void main(String[] args) {
+
+        byte a = 20;
+        byte b = 30;
+
+        int res = a * b;
+
+        System.out.println(res);
+    }
+}
