d4d5dd94ace7f0feee100aeff4e42ac1f5ba85f6
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-12 21:17:31 -0500

    Create Casting.java

---
+++ Casting.java
@@ -0,0 +1,13 @@
+class Casting {
+
+    public static void main(String[] args) {
+        int a = 257;
+        byte k = (byte) a;
+
+        System.out.println(a);
+
+        float f = 5.6f;
+        int t = (int) (f);
+        System.out.println(t);
+    }
+}
