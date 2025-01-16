aa3f4fe6f787dcbd79b833d6b1088ae6823110e2
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-16 15:30:02 -0500

    Create while.java

---
+++ while.java
@@ -0,0 +1,46 @@
+ //#17 While Loop in Java 
+
+class whil {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Hi");
+
+        }
+    }
+
+//keeps printing while 
+
+class whil {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 5) {
+            System.out.println("Hi " + i);
+            i++;
+        }
+    }
+}
+
+
+
+
+
+class whil {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 4) {
+            System.out.println("Hi " + i);
+            int j = 1;
+            while (j < 4) {
+
+                System.out.println("Hello");
+                j++;
+            }
+
+            i++;
+
+        }
+
+        System.out.println("Bye" + i);
+    }
+}
