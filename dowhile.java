cf7491d87879b72903fbac5aad22697c22976214
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-16 15:38:02 -0500

    Create dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+public class dowhil {
+
+    public static void main(String[] args) {
+
+        int i = 5;
+        do {
+
+            System.out.println("HI" + i);
+            i++;
+        } while (i <= 4);
+    }
+}
