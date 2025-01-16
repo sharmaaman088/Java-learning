be9e3900e62712be88290b3c1cce385843c7e341
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-16 16:09:44 -0500

    Create frloo.java

---
+++ frloo.java
@@ -0,0 +1,79 @@
+// you can enter 3 different statement in 1 for loop 
+
+i =1;
+i<3;
+i++
+
+
+public class fr {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++)
+
+        {
+            System.out.println("Hello" + i);
+
+        }
+
+    }
+}
+
+
+
+public class fr {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++)
+
+        {
+            System.out.println("Hello" + i);
+
+        }
+
+    }
+}
+
+
+
+
+
+public class fr {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++)
+
+        {
+            System.out.println("Hello" + i);
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
