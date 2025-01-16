181b1c4a95c37a8922dc516ebec32aa22f4de586
sharmaaman088 <90156384+sharmaaman088@users.noreply.github.com>
2025-01-16 16:10:05 -0500

    Create NestedFor.java

---
+++ NestedFor.java
@@ -0,0 +1,13 @@
+public class Nestedfor {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Day " + i);
+            for (int j = 1; j < 9; j++) {
+                System.out.println(" " + (j + 8) + " - " + (j + 9));
+            }
+
+        }
+    }
+}
